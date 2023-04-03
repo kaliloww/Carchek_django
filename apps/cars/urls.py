5f0789e6e51dde2a9606d87a657700ee9cf26c6d
Toktorov <ktoktorov2005@gmail.com>
2023-04-03 16:28:24 +0600

    add mixins for cars

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path
+from rest_framework import routers
 
-from apps.cars.views import CarAPIView, CarCreateAPIView, CarUpdateAPIView, CarDestroyAPIView
+from apps.cars.views import CarAPIViewSet
 
-urlpatterns = [
-    path('cars/', CarAPIView.as_view(), name = "api_cars"),
-    path('cars/create/', CarCreateAPIView.as_view(), name = "api_cars_create"),
-    path('cars/update/<int:pk>/', CarUpdateAPIView.as_view(), name = "api_cars_update"),
-    path('cars/destroy/<int:pk>/', CarDestroyAPIView.as_view(), name = "api_cars_destroy"),
-]
+router = routers.DefaultRouter()
+router.register('', CarAPIViewSet, "api_cars")
+
+urlpatterns = router.urls
--- apps/cars/views.py
+++ apps/cars/views.py
@@ -1,24 +1,15 @@
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin, RetrieveModelMixin
 
 from apps.cars.models import Car
 from apps.cars.serializers import CarSerializer
 
 # Create your views here.
-class CarAPIView(ListAPIView):
-    queryset = Car.objects.all() #SELECT * FROM cars_car;
-    serializer_class = CarSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['license_plate', ]
-
-class CarCreateAPIView(CreateAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
-
-class CarUpdateAPIView(UpdateAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
-
-class CarDestroyAPIView(DestroyAPIView):
+class CarAPIViewSet(GenericViewSet,
+                    ListModelMixin,
+                    RetrieveModelMixin,
+                    CreateModelMixin,
+                    UpdateModelMixin,
+                    DestroyModelMixin):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
